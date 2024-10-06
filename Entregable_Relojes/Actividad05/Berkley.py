026935d997b4e3c46b38c0dba73b724ebea8c300
Azrielx86 <edgar1569@outlook.com>
2024-10-06 17:47:07 -0600

    A05 Updated

--- Entregable_Relojes/Actividad05/Berkley.py
+++ Entregable_Relojes/Actividad05/Berkley.py
@@ -15,7 +15,15 @@ def format_time(t: float) -> str:
 
 
 def coordinator(pipes: list[tuple[Connection, Connection]]) -> None:
+    """
+    Función para el coordinador
+    :param pipes: Array con los canales de comunicación de los procesos.
+    El primer elemento es el canal hacia el nodo, el segundo es el canal
+    hacia el coordinador.
+    """
     local_times: list[float] = []
+
+    print("[Coordinator] Requesting times to nodes.")
     for node, _ in pipes:
         node.send(Messages.REQUEST_TIME)
 
@@ -26,28 +34,35 @@ def coordinator(pipes: list[tuple[Connection, Connection]]) -> None:
     avg_local_time = sum(local_times) / len(local_times)
     diffs = [avg_local_time - ltime for ltime in local_times]
 
-    print(f"Average time: {avg_local_time} | Diffs: {diffs}")
+    print(f"[Coordinator] Average time: {avg_local_time} | Diffs: {diffs}")
 
     for idx, (node, _) in enumerate(pipes):
         node.send(diffs[idx])
 
-    print("Corrections sended")
+    print("[Coordinator] Corrections sended")
     for node, _ in pipes:
         node.close()
 
 
-def node(pid: int, pipe: tuple[Connection, Connection]):
-    print(f"Process {pid}")
+def node(pid: int, pipe: tuple[Connection, Connection]) -> None:
+    """
+    Función para los nodos.
+    Se comunica con el coordinador para dar su hora, y luego recibir
+    el ajuste que se debe de aplicar.
+    :param pid: ID del proceso.
+    :param pipe: Canales de comunicación entre el proceso y el coordinador.
+    """
+    print(f"[{pid}] Process started.")
     _, coord = pipe
 
     if coord.recv() == Messages.REQUEST_TIME:
-        print("Requested...")
-        time.sleep(random.uniform(0.1, 2))
+        print(f"[{pid}] Time Requested...")
+        time.sleep(random.uniform(0.1, 1.5))
         local_time = time.time()
         coord.send(local_time)
         diff = coord.recv()
         new_time = local_time + diff
-        print(f"Process {pid}: Local Time: {local_time:.9f} | New: {new_time:.9f} | Diff: {diff:.9f}")
+        print(f"[{pid}] Local Time: {local_time:.9f} | New: {new_time:.9f} | Diff: {diff:.9f}")
 
 
 if __name__ == '__main__':
